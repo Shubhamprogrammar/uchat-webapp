06b0b64937a01390a32485e30812de76bd5db158
Apurv <apurvborkar56@gmail.com>
2025-12-16 20:44:35 +0530

    Implemented notifications using react-hot-toast

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import  {Routes,Route} from 'react-router-dom';
 import LandingPage from './pages/LandingPage.jsx';
+import MessagePage from './pages/MessagePage.jsx';
+import {Toaster} from 'react-hot-toast';
+
 
 function App() {
 
   return (
     <>
+    <Toaster position="top-center" reverseOrder={false} />
     <Routes>
       <Route path='/' element={<LandingPage/>}/>
+      <Route path='/message' element={<MessagePage/>}/>
     </Routes>
     </>
   )

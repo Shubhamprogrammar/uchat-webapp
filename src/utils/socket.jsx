4eb65954ddcd76b3ca73848dbc09d6b024a9beeb
Apurv <apurvborkar56@gmail.com>
2026-02-01 13:42:34 +0530

    socket production url updated

--- src/utils/socket.jsx
+++ src/utils/socket.jsx
@@ -2,6 +2,7 @@ import { io } from "socket.io-client";
 
 const socket = io(import.meta.env.VITE_BACKEND_URL, {
   autoConnect: false,
+   transports: ["websocket", "polling"],
 });
 
 export default socket;
